4607c17451ef67d45613e1e75218ca254768c823
Yota Toyama <raviqqe@gmail.com>
2023-11-11 00:47:22 +1100

    Fix

--- src/shuffle.ts
+++ src/shuffle.ts
@@ -1,3 +1,14 @@
+import { range } from "./range.js";
+
 export const shuffle = <T>(xs: T[]): T[] => {
-  const indexes = new Set(range(xs.length));
+  xs = [...xs];
+
+  for (const index of range(xs.length)) {
+    const targetIndex = index + Math.floor(Math.random() * (xs.length - index));
+    const x = xs[targetIndex]!;
+    xs[targetIndex] = xs[index]!;
+    xs[index] = x;
+  }
+
+  return xs;
 };
