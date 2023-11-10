34518db097188933a136a5eb3995cd16fbff27d8
Yota Toyama <raviqqe@gmail.com>
2023-11-11 00:34:15 +1100

    Sort by (#12)

---
+++ src/sort-by.test.ts
@@ -0,0 +1,25 @@
+import { expect, it } from "vitest";
+import { sortBy } from "./sort-by.js";
+
+it("sorts objects by a property", () => {
+  expect(sortBy([{ foo: 3 }, { foo: 1 }, { foo: 2 }], (x) => x.foo)).toEqual([
+    { foo: 1 },
+    { foo: 2 },
+    { foo: 3 },
+  ]);
+});
+
+it("reverses an array of numbers", () => {
+  expect(sortBy([1, 2, 3], (x) => -x)).toEqual([3, 2, 1]);
+});
+
+it("calculates a key only once", () => {
+  const called = new Set();
+
+  expect(
+    sortBy([1, 2, 3, 1, 2, 3], (x) => {
+      expect(called.has(x)).toBe(false);
+      return x;
+    }),
+  ).toEqual([1, 1, 2, 2, 3, 3]);
+});
---
+++ src/sort-by.ts
@@ -0,0 +1,14 @@
+export type SortKey = string | number | boolean | SortKey[];
+
+export const sortBy = <T>(xs: T[], f: (x: T) => SortKey): T[] => {
+  const keys = new Map<T, ReturnType<typeof f>>();
+  const g = (x: T) => {
+    if (!keys.has(x)) {
+      keys.set(x, f(x));
+    }
+
+    return keys.get(x)!;
+  };
+
+  return xs.sort((x, y) => (g(x) < g(y) ? -1 : 1));
+};
