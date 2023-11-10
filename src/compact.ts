e9d4799822841109e76ab5ecc44b528a430acfb6
Yota Toyama <raviqqe@gmail.com>
2023-11-10 19:22:47 +1100

    Fix

--- src/compact.ts
+++ src/compact.ts
@@ -1,6 +1,4 @@
-import { identity } from "./identity.js";
-
 export const compact = <T>(
   xs: T[],
 ): Exclude<T, 0 | "" | false | null | undefined>[] =>
-  xs.filter(identity) as Exclude<T, 0 | "" | false | null | undefined>[];
+  xs.filter(Boolean) as Exclude<T, 0 | "" | false | null | undefined>[];
