08d0d9a5137c19508a2693ad09a30cda0ed36951
Yota Toyama <raviqqe@gmail.com>
2023-11-10 16:30:39 +1100

    Fix

--- src/last.test.ts
+++ src/last.test.ts
@@ -1,8 +1,8 @@
 import { last } from "./last.js";
 import { it, expect } from "vitest";
 
-for (const [xs, y] of [] satisfies [number[], number][]) {
-  it(`finds the last element in an array ${JSON.strigify(xs)}`, () => {
-    expect(last([1, 2, 3])).toBe(3);
+for (const [xs, y] of [[[1, 2, 3], 3]] satisfies [number[], number][]) {
+  it(`finds the last element in an array ${JSON.stringify(xs)}`, () => {
+    expect(last(xs)).toBe(y);
   });
 }
