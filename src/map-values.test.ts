871237547e1ffde22a3ccc9cc36648549e4ce2fa
Yota Toyama <raviqqe@gmail.com>
2023-11-10 20:03:39 +1100

    Add mapValues (#6)

---
+++ src/map-values.test.ts
@@ -0,0 +1,13 @@
+import { it, expect } from "vitest";
+import { mapValues } from "./map-values.js";
+
+it("maps values in an object", () => {
+  expect(mapValues({ bar: 3, foo: 2 }, (x) => x * x)).toEqual({
+    bar: 9,
+    foo: 4,
+  });
+});
+
+it("maps values in an object into a different type", () => {
+  expect(mapValues({ bar: 3, foo: 2 }, String)).toEqual({ bar: "3", foo: "2" });
+});
