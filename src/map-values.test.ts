10e4b99a4aef43d060460d923cb0ca8d57c8466c
Yota Toyama <raviqqe@gmail.com>
2023-11-10 19:57:43 +1100

    Fix

--- src/map-values.test.ts
+++ src/map-values.test.ts
@@ -1,11 +1,13 @@
 import { it, expect } from "vitest";
 import { mapValues } from "./map-values.js";
 
-for (const [x, y] of [[{ foo: 2 }, { foo: 4 }]] satisfies [
-  Record<string, unknown>,
-  Record<string, unknown>,
-][]) {
-  it(`maps values in an object ${JSON.stringify(x)}`, () => {
-    expect(mapValues(x, (x) => x * x)).toEqual(y);
+it("maps values in an object", () => {
+  expect(mapValues({ foo: 2, bar: 3 }, (x) => x * x)).toEqual({
+    foo: 4,
+    bar: 9,
   });
-}
+});
+
+it("maps values in an object into a different type", () => {
+  expect(mapValues({ foo: 2, bar: 3 }, String)).toEqual({ foo: "2", bar: "3" });
+});
--- src/map-values.ts
+++ src/map-values.ts
@@ -1,10 +1,7 @@
-export const mapValues = <K extends string, V>(
-  x: Record<K, V>,
-  f: (x: V) => V,
-): Record<K, V> =>
+export const mapValues = <K extends string, V, W>(
+  x: { [k in K]: V },
+  f: (x: V) => W,
+): { [k: string]: W } =>
   Object.fromEntries(
-    [...Object.entries(x)].map(([key, value]: [string, unknown]) => [
-      key,
-      f(value as V),
-    ]),
-  ) as Record<K, V>;
+    [...Object.entries<V>(x)].map(([key, value]) => [key, f(value)]),
+  );
