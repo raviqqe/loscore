d1a95263dde78a3dd9ead832d2d9c2c38a630a6f
Yota Toyama <raviqqe@gmail.com>
2023-11-10 19:10:17 +1100

    Fix

--- src/compact.test.ts
+++ src/compact.test.ts
@@ -3,13 +3,18 @@ import { compact } from "./compact.js";
 
 for (const [xs, y] of [
   [[0], []],
+  [[""], []],
   [[undefined], []],
   [[null], []],
   [[false], []],
+  [
+    [-1, 0, 1],
+    [-1, 1],
+  ],
 ] satisfies [unknown[], unknown[]][]) {
   it(`finds the last element in an array ${JSON.stringify(xs)}`, () => {
     const zs: unknown[] = xs;
 
-    expect(compact(zs)).toBe(y);
+    expect(compact(zs)).toEqual(y);
   });
 }
