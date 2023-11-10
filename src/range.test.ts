792c4e6001d92e6773c33d1ca69680dbcb8f84be
Yota Toyama <raviqqe@gmail.com>
2023-11-10 21:25:01 +1100

    Fix

--- src/range.test.ts
+++ src/range.test.ts
@@ -8,3 +8,20 @@ it("iterates over a range", () => {
     expect(i).toEqual(x++);
   }
 });
+
+it("iterates over a range with a start", () => {
+  let x = 0;
+
+  for (const i of range(0, 10)) {
+    expect(i).toEqual(x++);
+  }
+});
+
+it("iterates over a range with a step", () => {
+  let x = 0;
+
+  for (const i of range(0, 10, 2)) {
+    expect(i).toEqual(x);
+    x += 2;
+  }
+});
