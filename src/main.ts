87325ff1fc0d84c5277ebb4a9a126d418bcd8170
Yota Toyama <raviqqe@gmail.com>
2023-11-12 07:56:17 +0800

    Fix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ export * from "./compact.js";
 export * from "./flow.js";
 export * from "./identity.js";
 export * from "./interleave.js";
+export * from "./is-upper-case.js";
+export * from "./is-lower-case.js";
 export * from "./is-boolean.js";
 export * from "./is-number.js";
 export * from "./is-string.js";
