d39205c158535b2d7db62c329c3af8a31434e0f4
Yota Toyama <raviqqe@gmail.com>
2023-11-11 13:54:54 +1100

    Fix

--- src/zip.test.ts
+++ src/zip.test.ts
@@ -2,7 +2,7 @@ import { expect, it } from "vitest";
 import { zip } from "./zip.js";
 
 it("zips two arrays", () => {
-  expect(zip([1, 2, 3], [4, 5, 6])).toEqual([
+  expect([...zip([1, 2, 3], [4, 5, 6])]).toEqual([
     [1, 4],
     [2, 5],
     [3, 6],
--- src/zip.ts
+++ src/zip.ts
@@ -1,3 +1,5 @@
-// TODO Use an iterator.
-export const zip = <T, S>(xs: T[], ys: S[]): [T, S][] =>
-  xs.slice(0, ys.length).map((x, index) => [x, ys[index]!]);
+export const zip = function* <T, S>(xs: T[], ys: S[]): Iterable<[T, S]> {
+  for (const [index, x] of xs.slice(0, ys.length).entries()) {
+    yield [x, ys[index]!];
+  }
+};
