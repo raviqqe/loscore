09ae0225cacc4da9866370342a60b97ba90a9471
Yota Toyama <raviqqe@gmail.com>
2023-11-12 08:28:43 +0800

    Fix

--- src/async.ts
+++ src/async.ts
@@ -5,5 +5,5 @@ export * from "./async/is-async-iterable.js";
 export * from "./async/map.js";
 export * from "./async/sleep.js";
 export * from "./async/slice.js";
-export * from "./async/to-flat-array.js";
 export * from "./async/stream.js";
+export * from "./async/to-flat-array.js";
