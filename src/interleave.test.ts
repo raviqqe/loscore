86a20ae042d50907e687d0917a499291bb6932cc
Yota Toyama <raviqqe@gmail.com>
2023-11-14 18:03:35 +0900

    Fix

--- src/interleave.test.ts
+++ src/interleave.test.ts
@@ -1,16 +1,20 @@
 import { describe, expect, it } from "vitest";
 import { interleave } from "./interleave.js";
 
+const interleaveToArray = (...args: Parameters<typeof interleave>) => [
+  ...interleave(...args),
+];
+
 describe(interleave.name, () => {
   it("interleaves a separator", async () => {
-    expect(interleave([], null)).toEqual([]);
-    expect(interleave([1], null)).toEqual([1]);
-    expect(interleave([1, 2], null)).toEqual([1, null, 2]);
-    expect(interleave([1, 2, 3], null)).toEqual([1, null, 2, null, 3]);
+    expect(interleaveToArray([], null)).toEqual([]);
+    expect(interleaveToArray([1], null)).toEqual([1]);
+    expect(interleaveToArray([1, 2], null)).toEqual([1, null, 2]);
+    expect(interleaveToArray([1, 2, 3], null)).toEqual([1, null, 2, null, 3]);
   });
 
   it("interleaves a separator with a function", async () => {
-    expect(interleave([1, 2, 3], (index) => index + 42)).toEqual([
+    expect(interleaveToArray([1, 2, 3], (index) => index + 42)).toEqual([
       1, 42, 2, 43, 3,
     ]);
   });
