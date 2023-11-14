a0b323e36e8d1c2bf0ca0ac442d6006602c62a98
Yota Toyama <raviqqe@gmail.com>
2023-11-14 23:48:47 +0900

    Fix

--- src/range.test.ts
+++ src/range.test.ts
@@ -29,3 +29,7 @@ it("iterates over a range with a step", () => {
 it("does not include an end", () => {
   expect([...range(3)]).toEqual([0, 1, 2]);
 });
+
+it("iterates over a range with a negative step", () => {
+  expect([...range(3, 0, -1)]).toEqual([3, 2, 1]);
+});
