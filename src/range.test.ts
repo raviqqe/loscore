c6ddde5eb2856a630745f7cd12919eb23df55764
Yota Toyama <raviqqe@gmail.com>
2023-11-14 23:52:36 +0900

    Fix

--- src/range.test.ts
+++ src/range.test.ts
@@ -13,10 +13,6 @@ it("iterates over a range with a step", () => {
   expect([...range(0, 10, 2)]).toEqual([0, 2, 4, 6, 8]);
 });
 
-it("does not include an end", () => {
-  expect([...range(3)]).toEqual([0, 1, 2]);
-});
-
 it("iterates over a range with a negative step", () => {
   expect([...range(3, 0, -1)]).toEqual([3, 2, 1]);
 });
