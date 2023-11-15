5d79bf4760c6f4d94f3d743536c5b4bb4d3b5669
Yota Toyama <raviqqe@gmail.com>
2023-11-15 14:15:54 +0900

    Fix

--- src/async/filter.ts
+++ src/async/filter.ts
@@ -1,6 +1,13 @@
-export const filter = async function* <T, S extends T = T>(
+export const filter: {
+  // TODO How can we not tell lie to the type system...?
+  <T, S extends T>(
+    iterable: AsyncIterable<T>,
+    check: (x: T) => x is S,
+  ): AsyncIterable<S>;
+  <T>(iterable: AsyncIterable<T>, check: (x: T) => unknown): AsyncIterable<T>;
+} = async function* <T>(
   iterable: AsyncIterable<T>,
-  check: (x: T) => x is S,
+  check: (x: T) => unknown,
 ): AsyncIterable<T> {
   for await (const x of iterable) {
     if (check(x)) {
