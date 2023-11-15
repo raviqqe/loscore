583448ed2ed2c4781845022ae7a3d68b18b6e1b4
Yota Toyama <raviqqe@gmail.com>
2023-11-15 14:17:18 +0900

    Fix filter function typing (#36)

--- src/filter.ts
+++ src/filter.ts
@@ -1,7 +1,8 @@
-export const filter = function* <T>(
-  xs: Iterable<T>,
-  f: (x: T) => unknown,
-): Iterable<T> {
+export const filter: {
+  // TODO How can we not tell lie to the type system...?
+  <T, S extends T = T>(xs: Iterable<T>, f: (x: T) => x is S): Iterable<S>;
+  <T>(xs: Iterable<T>, f: (x: T) => unknown): Iterable<T>;
+} = function* <T>(xs: Iterable<T>, f: (x: T) => unknown): Iterable<T> {
   for (const x of xs) {
     if (f(x)) {
       yield x;
