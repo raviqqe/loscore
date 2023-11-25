632f8ace0dc10e8f5c83c6a24794dc766d9a40a8
Yota Toyama <raviqqe@gmail.com>
2023-11-25 18:31:50 +0900

    Fix

--- src/group-by.test.ts
+++ src/group-by.test.ts
@@ -1,6 +1,16 @@
 import { expect, it } from "vitest";
 import { groupBy } from "./group-by.js";
 
-it("groups values", () => {
+it("groups values with number keys", () => {
   expect(groupBy([1, 2, 3, 4], (x) => x % 2)).toEqual({ 0: [2, 4], 1: [1, 3] });
+});
+
+it("groups values with string keys", () => {
+  expect(
+    groupBy(["apple", "pine", "lemon", "almond", "leek"], (x) => x[0] ?? ""),
+  ).toEqual({
+    a: ["apple", "almond"],
+    l: ["lemon", "leek"],
+    p: ["pine"],
+  });
 });
