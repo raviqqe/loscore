456c77fc05e4e09c8175d026907f67f70eb82837
Yota Toyama <raviqqe@gmail.com>
2023-11-25 13:01:39 +0900

    Fix

--- src/every.ts
+++ src/every.ts
@@ -1,9 +1,13 @@
-export const every = (xs: Iterable<unknown>) => {
+export const every = <T>(xs: Iterable<T>): T | boolean => {
+  let y: T | boolean = true;
+
   for (const x of xs) {
     if (!x) {
       return false;
     }
+
+    y = x;
   }
 
-  return true;
+  return y;
 };
--- src/some.test.ts
+++ src/some.test.ts
@@ -3,8 +3,10 @@ import { some } from "./some.js";
 
 it("checks if there is at least a truthy value", () => {
   expect(some([])).toBe(false);
+  expect(some([false])).toBe(false);
   expect(some([true])).toBe(true);
   expect(some([42])).toBe(42);
+  expect(some([false, false])).toBe(false);
   expect(some([false, true])).toBe(true);
   expect(some([false, 42])).toBe(42);
   expect(some([false, false, true])).toBe(true);
