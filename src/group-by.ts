8a06a54eaa283356d92280b53a3df6c2e6c0e94d
Yota Toyama <raviqqe@gmail.com>
2023-11-25 18:32:13 +0900

    Fix

--- src/group-by.ts
+++ src/group-by.ts
@@ -2,7 +2,7 @@ export const groupBy = <T, K extends string | number | symbol>(
   xs: Iterable<T>,
   f: (x: T) => K,
 ): Record<K, T[]> => {
-  let ys = {} as Record<K, T[]>;
+  const ys = {} as Record<K, T[]>;
 
   for (const x of xs) {
     (ys[f(x)] ??= []).push(x);
