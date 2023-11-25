d6a2545e91969fff49bf86951940dc58ab3c1ae6
Yota Toyama <raviqqe@gmail.com>
2023-11-25 18:35:44 +0900

    Fix

--- src/group-by.test.ts
+++ src/group-by.test.ts
@@ -2,6 +2,7 @@ import { expect, it } from "vitest";
 import { groupBy } from "./group-by.js";
 
 it("groups values with number keys", () => {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
   expect(groupBy([1, 2, 3, 4], (x) => x % 2)).toEqual({ 0: [2, 4], 1: [1, 3] });
 });
 
