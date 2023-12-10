039f727f5d12deeb39481885036fcef378fa5d62
Yota Toyama <raviqqe@gmail.com>
2023-12-10 22:38:46 +0900

    Fix

--- src/interleave.ts
+++ src/interleave.ts
@@ -1,11 +1,11 @@
 export const interleave = function* <T, S>(
-  xs: T[],
+  array: T[],
   separator: S | ((index: number) => S),
 ): Iterable<T | S> {
-  for (const [index, value] of xs.entries()) {
+  for (const [index, value] of array.entries()) {
     yield value;
 
-    if (index !== xs.length - 1) {
+    if (index !== array.length - 1) {
       yield separator instanceof Function ? separator(index) : separator;
     }
   }
