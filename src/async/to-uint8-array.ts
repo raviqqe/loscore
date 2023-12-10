6bfa819079323b0080bf868f59cb94e85d72ec5d
Yota Toyama <raviqqe@gmail.com>
2023-12-10 21:21:09 +0900

    Fix

--- src/async/to-uint8-array.ts
+++ src/async/to-uint8-array.ts
@@ -1,12 +1,21 @@
+import { sum } from "../sum.js";
+import { map } from "../map.js";
 import { toIterable } from "./to-iterable.js";
 
 export const toStream = async (
   stream: ReadableStream<Uint8Array>,
 ): Promise<Uint8Array> => {
-  const array = [];
+  const chunks = [];
 
   for await (const chunk of toIterable(stream)) {
-    array.push(chunk);
+    chunks.push(chunk);
+  }
+
+  const length = sum(map(chunks, (chunk) => chunk.length));
+  const array = new Uint8Array(length);
+
+  for (const chunk of chunks) {
+    array.set(chunk);
   }
 
   return array;
