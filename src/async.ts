04da5d45574fc85c4d971b21831b8d2d992e20a8
Yota Toyama <raviqqe@gmail.com>
2023-12-10 22:46:54 +0900

    Fix

--- src/async.ts
+++ src/async.ts
@@ -13,4 +13,5 @@ export * from "./async/to-byte-stream.js";
 export * from "./async/to-iterable.js";
 export * from "./async/to-stream.js";
 export * from "./async/to-string-stream.js";
+export * from "./async/to-string.js";
 export * from "./async/to-uint8-array.js";
