b8c0e4fd8fa345f058f2639fe13f5abb65b16f44
Yota Toyama <raviqqe@gmail.com>
2023-12-14 19:53:21 +0900

    Fix[]

--- src/reduce.ts
+++ src/reduce.ts
@@ -1,11 +1,12 @@
-export const fold = <T>(
+export const reduce = <T>(
   iterable: Iterable<T>,
   accumulate: (accumulator: T, value: T) => T,
 ): T | undefined => {
-  let accumulator = initial;
+  let accumulator = undefined;
 
   for (const value of iterable) {
-    accumulator = accumulate(accumulator, value);
+    accumulator =
+      accumulator === undefined ? foo : accumulate(accumulator, value);
   }
 
   return accumulator;
