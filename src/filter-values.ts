f4a9d3129c0c69b2c3c093fff609b993947b8562
Yota Toyama <raviqqe@gmail.com>
2023-12-15 09:14:01 +0900

    Fix

--- src/filter-values.ts
+++ src/filter-values.ts
@@ -1,7 +1,19 @@
-export const filterValues = <K extends number | string | symbol, V, W>(
+import { filter } from "./filter.js";
+
+// TODO How can we not tell lie to the type system...?
+export const filterValues: {
+  <K extends string | number | symbol, V, W extends V = V>(
+    object: Record<K, V>,
+    test: (value: V) => value is W,
+  ): Record<string, V>;
+  <K extends string | number | symbol, V>(
+    object: Record<K, V>,
+    test: (value: V) => unknown,
+  ): Record<string, V>;
+} = <K extends string, V>(
   object: Record<K, V>,
-  convert: (value: V) => W,
-): Record<string, W> =>
-  Object.fromEntries(
-    [...Object.entries<V>(object)].map(([key, value]) => [key, convert(value)]),
+  test: (value: V) => unknown,
+): Record<string, V> =>
+  Object.fromEntries<V>(
+    filter(Object.entries<V>(object), ([, value]) => test(value)),
   );
