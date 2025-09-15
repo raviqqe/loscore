2d24efe5ed7fe4be5df594ca749168d794838b99
Yota Toyama <raviqqe@gmail.com>
2025-09-14 19:13:35 -0700

    Remove deprecated API (#688)

--- src/async/map-stream.ts
+++ src/async/map-stream.ts
@@ -1,8 +1,7 @@
 import { map } from "./map.js";
-import { toIterable } from "./to-iterable.js";
 import { toStream } from "./to-stream.js";
 
 export const mapStream = <T, S>(
   stream: ReadableStream<T>,
   callback: (value: T) => S,
-): ReadableStream<S> => toStream(map(toIterable(stream), callback));
+): ReadableStream<S> => toStream(map(stream, callback));
--- src/async/to-stream.test.ts
+++ src/async/to-stream.test.ts
@@ -1,19 +1,16 @@
 import { expect, it } from "vitest";
 
-import { toIterable } from "./to-iterable.js";
 import { toStream } from "./to-stream.js";
 
 it("converts iterable into a stream", async () => {
   expect(
     await Array.fromAsync(
-      toIterable(
-        toStream(
-          (async function* () {
-            yield 1;
-            yield 2;
-            yield 3;
-          })(),
-        ),
+      toStream(
+        (async function* () {
+          yield 1;
+          yield 2;
+          yield 3;
+        })(),
       ),
     ),
   ).toEqual([1, 2, 3]);
